ed13321df88a5e38b48629948e12b05e5aae67bd
AlexTEXHAPb <130362589+AlexTEXHAPb@users.noreply.github.com>
2023-06-30 06:10:23 +1000

    Добавил файл Program.cs
    Написал и добавил программу, решающую поставленную задачу

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿string[] array = new string[5] { "Привет!", "мой", "родной", "мир", ":-)" };
+int k = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+    if (array[i].Length <= 3)
+    {
+        array[k] = array[i];
+        k++;
+    }
+}
+Console.WriteLine();
+string[] newarray = new string[k];
+for (int i = 0; i < k; i++)
+{
+    newarray[i] = array[i];
+    Console.Write($"{newarray[i]} ");
+}
