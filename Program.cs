d690aae1b121ccc6e2045f314577b60bac640881
Alex <lex777@rambler.ru>
2023-06-30 17:16:35 +1000

    Написал программу. Работает :)

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = new string[9]
+{ "Привет!", "Mой", "родной", "мир", ":-)", "Я", "и", "ты", "вместе!" };
+int k = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+    if (array[i].Length <= 3)
+    {
+        array[k] = array[i];
+        k++;
+    }
+}
+Console.WriteLine();
+string[] newarray = new string[k];
+for (int i = 0; i < k; i++)
+{
+    newarray[i] = array[i];
+    Console.Write($"{newarray[i]} ");
+}
